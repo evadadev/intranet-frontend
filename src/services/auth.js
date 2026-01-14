9a35cad9b31094e80c28aedbeb2d7ff824dc9e6f
Eva Díaz <dev.evada@gmail.com>
2026-01-14 16:06:38 +0100

    fix: call to post

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,16 +1,16 @@
-import RequestApi from './RequestApi.js'
+import { post } from './RequestApi.js'
 
 export async function setRegister(body) {
-  const response = await RequestApi.post('register', body)
+  const response = await post('register', body)
   return response
 }
 
 export async function setLogin(body) {
-  const response = await RequestApi.post('login', body)
+  const response = await post('login', body)
   return response
 }
 
 export async function setLogout(body) {
-  const response = await RequestApi.post('logout', body)
+  const response = await post('logout', body)
   return response
 }
