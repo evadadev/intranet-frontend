7a6980153636215e24f048eedccf719fc106359b
Eva Díaz <dev.evada@gmail.com>
2026-01-14 11:31:52 +0100

    feat: create layoutLogin (#14)

---
+++ src/components/ui/TitleForm.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  titleText: {
+    type: String,
+    default: '',
+  },
+  subtitleText: {
+    type: String,
+    default: '',
+  },
+})
+</script>
+
+<template>
+  <div class="pb-3">
+    <h2 class="text-3xl font-bold">{{ titleText }}</h2>
+    <p class="text-sm">{{ subtitleText }}</p>
+  </div>
+</template>
