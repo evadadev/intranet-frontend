50561bbde34a4981c12f857a7cb44cb5074eb88c
Eva Díaz <dev.evada@gmail.com>
2026-01-19 15:30:15 +0100

    feat: create news layouts to layoutHome

---
+++ src/components/ui/LayoutHome.vue
@@ -0,0 +1,23 @@
+<script setup>
+import LayoutHeaders from '@/components/ui/LayoutHeaders.vue'
+import LayoutSidebar from '@/components/ui/LayoutSidebar.vue'
+import LayoutMain from '@/components/ui/LayoutMain.vue'
+
+import CardBase from '@/components/ui/CardBase.vue'
+</script>
+<template>
+  <div class="bg-neutral-100 grid grid-cols-[200px_1fr] gap-4 p-2 min-h-screen">
+    <div class="bg-neutral-100 grid grid-cols-[200px_1fr] gap-4 p-2 min-h-screen">
+      <LayoutSidebar titleSidebar="Portal de empleados" />
+      <section class="flex flex-col">
+        <LayoutHeaders tituloHeader="MI PORTAL" />
+        <LayoutMain>
+          <CardBase class="w-80 h-80" tituloCard="Tú equipo" avatar />
+          <CardBase class="w-80 h-80" tituloCard="Mis tareas" />
+          <CardBase class="w-80 h-80" tituloCard="Mis documentos" />
+        </LayoutMain>
+      </section>
+    </div>
+    <slot />
+  </div>
+</template>
---
+++ src/views/DocumentosView.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { useRouter } from 'vue-router'
+
+import CardBase from '@/components/ui/CardBase.vue'
+import BtnBase from '@/components/ui/BtnBase.vue'
+
+const router = useRouter()
+
+const btnVolverInicio = () => {
+  router.push('/')
+}
+</script>
+
+<template>
+  <div>
+    <h1>Mis documentos</h1>
+    <h2>meter el perfil del empleado</h2>
+    <BtnBase textBtn="Nuevo documento" />
+    <CardBase class="w-80 h-80" tituloCard="Mis documentos" />
+    <BtnBase
+      textBtn="Incio"
+      class="absolute bottom-1 left-2 w-[110px] h-8"
+      @click="btnVolverInicio"
+    />
+  </div>
+</template>
