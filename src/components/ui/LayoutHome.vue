8d72c6db9993015ffdd31ed3e5eb8500574be5cd
Eva Díaz <dev.evada@gmail.com>
2026-01-20 10:43:22 +0100

    fix: remove div to LayoutHome.vue #20

--- src/components/ui/LayoutHome.vue
+++ src/components/ui/LayoutHome.vue
@@ -7,17 +7,15 @@ import CardBase from '@/components/ui/CardBase.vue'
 </script>
 <template>
   <div class="bg-neutral-100 grid grid-cols-[200px_1fr] gap-4 p-2 min-h-screen">
-    <div class="bg-neutral-100 grid grid-cols-[200px_1fr] gap-4 p-2 min-h-screen">
-      <LayoutSidebar titleSidebar="Portal de empleados" />
-      <section class="flex flex-col">
-        <LayoutHeaders tituloHeader="MI PORTAL" />
-        <LayoutMain>
-          <CardBase class="w-80 h-80" tituloCard="Tú equipo" avatar />
-          <CardBase class="w-80 h-80" tituloCard="Mis tareas" />
-          <CardBase class="w-80 h-80" tituloCard="Mis documentos" />
-        </LayoutMain>
-      </section>
-    </div>
-    <slot />
+    <LayoutSidebar titleSidebar="Portal de empleados" />
+    <section class="flex flex-col">
+      <LayoutHeaders tituloHeader="MI PORTAL" />
+      <LayoutMain>
+        <CardBase class="w-80 h-80" tituloCard="Tú equipo" avatar />
+        <CardBase class="w-80 h-80" tituloCard="Mis tareas" />
+        <CardBase class="w-80 h-80" tituloCard="Mis documentos" />
+      </LayoutMain>
+    </section>
   </div>
+  <slot />
 </template>
