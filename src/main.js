2ccad984260e326771ecac354b186f1e994a28ec
Eva Díaz <dev.evada@gmail.com>
2026-01-20 16:25:51 +0100

    chore: install fontAwesome (#22)

--- src/main.js
+++ src/main.js
@@ -5,11 +5,43 @@ import App from './App.vue'
 import router from './router'
 import i18n from './plugins/i18n'
 import './style.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {
+  faAddressCard,
+  faAngleDown,
+  faBell,
+  faCalendarCheck,
+  faChartSimple,
+  faClipboardCheck,
+  faClock,
+  faFile,
+  faGhost,
+  faPeopleGroup,
+  faUser,
+  faXmark,
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(
+  faUser,
+  faAngleDown,
+  faXmark,
+  faBell,
+  faFile,
+  faAddressCard,
+  faGhost,
+  faCalendarCheck,
+  faClock,
+  faClipboardCheck,
+  faPeopleGroup,
+  faChartSimple,
+)
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(i18n)
+app.component('FontAwesomeIcon', FontAwesomeIcon)
 
 app.mount('#app')
